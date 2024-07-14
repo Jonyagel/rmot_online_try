063bbd1cbcab543f857c5eef2a47a9a69289ae50
Jonyagel <139068520+Jonyagel@users.noreply.github.com>
2024-07-14 13:47:53 +0300

    Delete src/app/forum directory

--- src/app/forum/comment/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function commentPage() {
-  return (
-    <div>commentPage</div>
-  )
-}
--- src/app/forum/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Loading() {
-  return (
-    <div className='container text-center'>
-      <h2>Loading...</h2>
-    </div>
-  )
-}
