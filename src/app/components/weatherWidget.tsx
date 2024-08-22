a2298e73dc0005ca6c203b6f01f4fde2f6bafa3d
Jonyagel <139068520+Jonyagel@users.noreply.github.com>
2024-08-22 03:13:32 +0300

    Update weatherWidget.tsx

--- src/app/components/weatherWidget.tsx
+++ src/app/components/weatherWidget.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState, useRef } from 'react';
 import { FaTimes } from 'react-icons/fa';
-import './WeatherWidget.css';
+import './weatherWidget.css';
 
 interface WeatherData {
   main: {
