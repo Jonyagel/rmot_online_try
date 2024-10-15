b029cfb00dc6d51f8d9600fb9c73e6ca3f3a1527
Jonyagel <139068520+Jonyagel@users.noreply.github.com>
2024-10-15 20:17:35 +0300

    Add files via upload

--- src/app/home/components/lottie.tsx
+++ src/app/home/components/lottie.tsx
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLottie, useLottieInteractivity } from "lottie-react";
 
-
-
-
 function Lottie(props:any) {
+    const [height, setHeight] = useState(250); // הוספת מצב לגובה
+
+    useEffect(() => {
+        // קביעת הגובה בהתאם לגודל המסך
+        const updateHeight = () => {
+            setHeight(window.innerWidth < 768 ? 130 : 250);
+        };
+        updateHeight(); // עדכון גובה בהתחלה
+        window.addEventListener('resize', updateHeight); // הוספת מאזין לאירוע שינוי גובה
+
+        return () => {
+            window.removeEventListener('resize', updateHeight); // ניקוי המאזין
+        };
+    }, []);
+
     const lottieOptions = {
         animationData: props.lottieNmae,
         autoplay: false,
     }
-    const lottieObj = useLottie(lottieOptions, { height:250 })
+    const lottieObj = useLottie(lottieOptions, { height }) // שימוש בגובה מהמצב
     const lottieAnimation = useLottieInteractivity({
         lottieObj,
         mode: 'scroll',
         actions: [{
-            visibility: [0.1, 0.5],
+            visibility: [0.1, 0.4],
             type: 'seek',
             frames: [0, 25]
         }]
