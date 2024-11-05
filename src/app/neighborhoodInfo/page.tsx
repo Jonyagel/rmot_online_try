cc1a9e61ca02c528aab0c50dc6366060a496bac0
Jonyagel <139068520+Jonyagel@users.noreply.github.com>
2024-11-05 15:58:57 +0200

    Add files via upload

--- src/app/neighborhoodInfo/page.tsx
+++ src/app/neighborhoodInfo/page.tsx
@@ -2,42 +2,31 @@ import React from 'react'
 import AcordionShops from './components/info'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
 
-export const dynamic = 'force-dynamic';
-
-
-export default async function NeighborhoodInfo() {
-
-    const doApi = async () => {
-        let url = `${process.env.NEXT_PUBLIC_API_URL}/api/shops`;
-        const resp = await fetch(url, { cache: 'no-store' })
+export const getStaticProps: GetStaticProps = async () => {
+    const doApi = async (endpoint: string) => {
+        const url = `${process.env.NEXT_PUBLIC_API_URL}/api/${endpoint}`;
+        const resp = await fetch(url, { cache: 'no-store' });
         const data = await resp.json();
-        console.log(data);
         return data;
-    }
+    };
 
-    const doApiGmach = async () => {
-        let url = `${process.env.NEXT_PUBLIC_API_URL}/api/gmach`;
-        const resp = await fetch(url, { cache: 'no-store' })
-        const data = await resp.json();
-        console.log(data);
-        // setCardsAr(data)
-        return data;
-    }
+    const initialDataShop = await doApi('shops');
+    const initialDataGmach = await doApi('gmach');
+    const initialDataMosads = await doApi('mosads');
 
-    const doApiMosads = async () => {
-        let url = `${process.env.NEXT_PUBLIC_API_URL}/api/mosads`;
-        const resp = await fetch(url, { cache: 'no-store' })
-        const data = await resp.json();
-        console.log(data);
-        // setCardsAr(data)
-        return data;
-    }
+    return {
+        props: {
+            initialDataShop,
+            initialDataGmach,
+            initialDataMosads,
+        },
+        revalidate: 86400, // עדכון כל 24 שעות (86400 שניות)
+    };
+};
 
-    const initialDataShop = await doApi();
-    const initialDataGmach = await doApiGmach();
-    const initialDataMosads = await doApiMosads();
-
+export default function NeighborhoodInfo({ initialDataShop, initialDataGmach, initialDataMosads }:any) {
     return (
         <>
             <Head>
@@ -50,14 +39,10 @@ export default async function NeighborhoodInfo() {
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://ramot-online-try.vercel.app/neighborhoodInfo" />
                 <meta property="og:image" content="https://asset.cloudinary.com/dglbrzbi1/52834b06603fe25209ec481073d6b5aa" />
-                {/* הוספת תגיות נוספות */}
             </Head>
             <div className='' style={{ minHeight: '100vh' }}>
-                {/* <StripCarousel /> */}
-                {/* <CardCategory /> */}
                 <AcordionShops shopsData={initialDataShop} gmachData={initialDataGmach} MosadsData={initialDataMosads} />
             </div>
         </>
-
-    )
+    );
 }
