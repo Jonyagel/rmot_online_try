75f16450f7917609347def0b298b779e80af5955
Jonyagel <139068520+Jonyagel@users.noreply.github.com>
2024-11-19 01:04:56 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,6 +66,16 @@ export default function RootLayout({
               </AuthProvider>
               <Analytics />
               <SpeedInsights />
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:5212525,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
               <script src="https://cdn.enable.co.il/licenses/enable-L29851bpdrqshwli-0824-64368/init.js"></script>
             </ContextProvider>
         </body>
