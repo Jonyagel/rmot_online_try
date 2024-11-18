3cd0c33797bf1e9ed0e35d759bc24efc85fa5f88
Jonyagel <139068520+Jonyagel@users.noreply.github.com>
2024-11-19 01:08:47 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,12 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import './bootstrap.rtl.min.css';
 import Header from "./header/page";
-import localFont from 'next/font/local'
-import { Analytics } from "@vercel/analytics/react"
+import localFont from 'next/font/local';
+import { Analytics } from "@vercel/analytics/react";
 import Footer from "./footer/page";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import SessionWrapper from "@/components/SessionWrapper";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google';
 import { ContextProvider } from './context/appContext';
 import 'react-toastify/dist/ReactToastify.css';
 import FooterNav from "./components/footerNav";
@@ -16,15 +16,12 @@ import { Toaster } from "react-hot-toast";
 import { AuthProvider } from './context/AuthContext';
 import ChatBot from './components/ChatBot/ChatBot';
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "על המקום",
-  description: "על המקום היא פלטפורמה לקידום והכרת המקום בו אתם חיים וגרים. כאן תוכלו למצוא מידע על השכונה שלכם, על המקומות המעניינים בה, ועל האנשים שחיים בה.",
+  description: "על המקום היא פלטפורמה לקידום והכרת המקום בו אתם חיים וגרים. כאן תוכלו למצוא מידע על השכונה שלכם, על המ�[...] 
 };
-
-
 
 const assistantFont = localFont({
   src: [
@@ -44,7 +41,7 @@ const assistantFont = localFont({
       style: 'normal',
     },
   ],
-})
+});
 
 export default function RootLayout({
   children,
@@ -55,29 +52,29 @@ export default function RootLayout({
     <SessionWrapper>
       <html lang="he" dir="rtl" className={assistantFont.className} >
         <body className={inter.className}>
-            <ContextProvider>
+          <ContextProvider>
             <AuthProvider>
-            <Toaster position="top-center" />
+              <Toaster position="top-center" />
               <Header />
               {children}
               <FooterNav />
               <Footer />
               {/* <ChatBot /> */}
-              </AuthProvider>
-              <Analytics />
-              <SpeedInsights />
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:5212525,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
-              <script src="https://cdn.enable.co.il/licenses/enable-L29851bpdrqshwli-0824-64368/init.js"></script>
-            </ContextProvider>
+            </AuthProvider>
+            <Analytics />
+            <SpeedInsights />
+            <script src="https://cdn.enable.co.il/licenses/enable-L29851bpdrqshwli-0824-64368/init.js"></script>
+            <script dangerouslySetInnerHTML={{ __html: `
+                (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:5212525,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            ` }} />
+          </ContextProvider>
         </body>
         <GoogleAnalytics gaId="G-7P3W0FCXJ2" />
       </html>
